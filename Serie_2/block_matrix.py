54110c13b944e02dd366956e57bd41872071cfc8
bresslem <bresslem@users.noreply.github.com>
2019-11-29 19:13:34 +0100

    clean code

--- Serie_2/block_matrix.py
+++ Serie_2/block_matrix.py
@@ -1,8 +1,10 @@
 """
 Author: Bressler_Marisa, Jeschke_Anne
 Date: 2019_11_13
+
+Creates the block matrix required to solve the discrete Poisson-Problem using
+finite differences and analyzes the amount of space needed.
 """
-# pylint: disable=invalid-name
 import scipy.sparse as sps
 import numpy as np
 from matplotlib import use
@@ -26,11 +28,11 @@ class BlockMatrix:
     n (int): Number of intervals in each dimension
     """
 
-    def __init__(self, d, n):
-        self.d = d
-        self.n = n
+    def __init__(self, d, n): #pylint: disable=invalid-name
+        self.d = d #pylint: disable=invalid-name
+        self.n = n #pylint: disable=invalid-name
 
-    def get_A_l(self, l):
+    def get_matrix_l(self, l): #pylint: disable=invalid-name
         """ Returns the block matrix at index l as sparse matrix.
 
         Parameters
@@ -42,42 +44,54 @@ class BlockMatrix:
         (scipy.sparse.csr_matrix): block_matrix in a sparse data format
         """
         if l == 1:
+            # base case: creating the matrix for d=1
             k = np.array([-np.ones(self.n-2), np.full((self.n-1), 2*self.d), -np.ones(self.n-2)])
             offset = [-1, 0, 1]
-            A_1 = sps.diags(k, offset).toarray()
-            return sps.csr_matrix(A_1)
+            return sps.csr_matrix(sps.diags(k, offset).toarray())
         else:
             if self.n == 2:
-                A_l = sps.csr_matrix([2*l])
+                # if n is 2 the matrix contains only one value
+                matrix_l = sps.csr_matrix([2*l])
             else:
-                A_prev = self.get_A_l(l-1)
-                dim = A_prev.shape[0]
-                I_neg = -1*sps.identity(dim, format='csr')
+                # recursion step: creating the previous matrix
+                matrix_prev = self.get_matrix_l(l-1)
+                dim = matrix_prev.shape[0]
+                # creating the negativ identity matrix and a zero matrix the size
+                # of the previous matrix
+                identity_neg = -1*sps.identity(dim, format='csr')
                 zeroes = sps.csr_matrix((dim, dim))
 
-                A_l = sps.hstack([A_prev, I_neg], format='csr')
+                # creating the first row of the block matrix, consisting of the previous matrix,
+                # a negative identity and the rest are zeroes
+                matrix_l = sps.hstack([matrix_prev, identity_neg], format='csr')
 
-                for i in range(((self.n-1)) - 2):
-                    A_l = sps.hstack([A_l, zeroes], format='csr')
+                for _ in range(((self.n-1)) - 2):
+                    matrix_l = sps.hstack([matrix_l, zeroes], format='csr')
 
+                # creating the following rows from the correct amount of zeroes,
+                # the negative identity, the previous matrix, the negative identity again
+                # and the rest zeroes and adding it to the bottom of the new matrix
                 for i in range((self.n-1) - 2):
-                    A_row = sps.csr_matrix((dim, 0))
+                    matrix_row = sps.csr_matrix((dim, 0))
                     for _ in range(i):
-                        A_row = sps.hstack([A_row, zeroes], format='csr')
-                    A_row = sps.hstack([A_row, I_neg, A_prev, I_neg], format='csr')
+                        matrix_row = sps.hstack([matrix_row, zeroes], format='csr')
+                    matrix_row = sps.hstack([matrix_row, identity_neg, matrix_prev, identity_neg],
+                                            format='csr')
                     for _ in range(((self.n-1) - 3) - i):
-                        A_row = sps.hstack([A_row, zeroes], format='csr')
-                    A_l = sps.vstack([A_l, A_row], format='csr')
+                        matrix_row = sps.hstack([matrix_row, zeroes], format='csr')
+                    matrix_l = sps.vstack([matrix_l, matrix_row], format='csr')
 
-                A_row = sps.csr_matrix((dim, 0))
+                # creating the last row consiting of zeroes, a negative identity and
+                # the previous matrix
+                matrix_row = sps.csr_matrix((dim, 0))
 
                 for i in range((self.n-1) - 2):
-                    A_row = sps.hstack([A_row, zeroes], format='csr')
+                    matrix_row = sps.hstack([matrix_row, zeroes], format='csr')
 
-                A_row = sps.hstack([A_row, I_neg, A_prev], format='csr')
-                A_l = sps.vstack([A_l, A_row], format='csr')
+                matrix_row = sps.hstack([matrix_row, identity_neg, matrix_prev], format='csr')
+                matrix_l = sps.vstack([matrix_l, matrix_row], format='csr')
 
-            return A_l
+            return matrix_l
 
     def get_sparse(self):
         """ Returns the block matrix as sparse matrix.
@@ -86,7 +100,7 @@ class BlockMatrix:
         -------
         (scipy.sparse.csr_matrix): block_matrix in a sparse data format
         """
-        return self.get_A_l(self.d)
+        return self.get_matrix_l(self.d)
 
     def eval_zeros(self):
         """ Returns the (absolute and relative) numbers of (non-)zero elements
@@ -112,7 +126,19 @@ class BlockMatrix:
 
 
 def plot_zeros(n_array):
+    """
+    Plots the amount of non zeros values contained in the block matrix for each dimension
+    and a given array of n's.
 
+    Parameters
+    ----------
+    n_array (list of ints): The n's for which to plot the non-zeroes and total values.
+
+    Returns
+    -------
+    (scipy.sparse.csr_matrix): block_matrix in a sparse data format
+    """
+    # pylint: disable=invalid-name
     for d in [1, 2, 3]:
         non_zeros = []
         absolute_values = []
@@ -130,13 +156,26 @@ def plot_zeros(n_array):
         plt.show()
 
 def plot_relative_zeroes(n_array):
+    """
+    Plots the amount of non zeros values contained in the block matrix for each
+    dimension and a given array of n's.
 
+    Parameters
+    ----------
+    n_array (list of ints): The n's for which to plot the non-zeroes and total values.
+
+    Returns
+    -------
+    (scipy.sparse.csr_matrix): block_matrix in a sparse data format
+    """
+    # pylint: disable=invalid-name
     for d in [1, 2, 3]:
         rel_non_zeros = []
         for n in n_array:
             matrix = BlockMatrix(d, n)
             rel_non_zeros.append(matrix.eval_zeros()[2])
-            print("d:", d, "; n:", n, ":", matrix.eval_zeros()[2], matrix.eval_zeros()[0])
+            print("d:", d, "; n:", n, "; relative non zeros:",
+                  matrix.eval_zeros()[2], "; absolute non zeros:", matrix.eval_zeros()[0])
         plt.plot(n_array, rel_non_zeros, "bo", label='relative number of non zero values')
         plt.xlabel('$n$')
         plt.title('d = ' + str(d))
@@ -145,10 +184,10 @@ def plot_relative_zeroes(n_array):
         plt.show()
 
 def main():
-    """ Main function to test the BlockMatrix class.
+    """ Main function to use the BlockMatrix class.
     """
     plot_zeros(range(2, 11))
-    plot_relative_zeroes(range(2,11))
+    plot_relative_zeroes(range(2, 11))
 
 if __name__ == "__main__":
     main()
