9c23857af2abf17779738289d2de94924e66b975
Marisa Bressler <bresslem@sulu.mathematik.hu-berlin.de>
2020-01-07 11:11:25 +0100

    init

---
+++ Serie_4/least_squares.py
@@ -0,0 +1,15 @@
+"""
+Author: Bressler_Marisa, Jeschke_Anne
+Date: 2020_01_03
+
+Main program to demonstrate the functionality of our modules.
+"""
+
+
+def main():
+    """ Main function to demonstrate the functionality of our modules.
+    """
+
+
+if __name__ == "__main__":
+    main()
